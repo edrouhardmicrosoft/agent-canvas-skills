146eb6880f6d2775573f41bc199e1b1c7c60f3ca
Erik Drouhard (MSFT) <erikdrouhard@microsoft.com>
2026-01-20 16:02:27 -0500

    fix(page): revert heading text to "hello world"

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-[72px] font-black mb-8 p-[52px] rounded-[40px]">Sup bro</h1>
+      <h1 className="text-[72px] font-black mb-8 p-[52px] rounded-[40px]">hello world</h1>
     </main>
   );
 }
